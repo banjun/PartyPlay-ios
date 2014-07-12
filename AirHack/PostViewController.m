5ce6e66239fa5b85cb087a5bb54e4be8063b1c91
banjun <banjun@gmail.com>
2014-07-12 18:35:22 +0900

    remove exported cache for overwriting export

--- AirHack/PostViewController.m
+++ AirHack/PostViewController.m
@@ -15,6 +15,7 @@
 
 @property (nonatomic) UIButton *postButton;
 @property (nonatomic) UITextField *urlField;
+@property (nonatomic) UITextField *titleField;
 
 @end
 
@@ -33,18 +34,31 @@ static NSString * const kPostURLKey = @"postURL";
     self.urlField.borderStyle = UITextBorderStyleRoundedRect;
     self.urlField.text = [[NSUserDefaults standardUserDefaults] stringForKey:kPostURLKey];
     
+    self.titleField = [[UITextField alloc] initWithFrame:CGRectZero];
+    
     self.postButton = [UIButton buttonWithType:UIButtonTypeRoundedRect];
     [self.postButton setTitle:@"Push Song" forState:UIControlStateNormal];
     [self.postButton addTarget:self action:@selector(retrieveCurrentMediaItem) forControlEvents:UIControlEventTouchUpInside];
     
-    NSDictionary *views = NSDictionaryOfVariableBindings(_urlField, _postButton);
+    NSDictionary *views = NSDictionaryOfVariableBindings(_urlField, _titleField, _postButton);
     for (UIView *v in views.allValues) {
         v.translatesAutoresizingMaskIntoConstraints = NO;
         [self.view addSubview:v];
     }
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_urlField]-|" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_titleField]-|" options:0 metrics:nil views:views]];
     [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"H:|-[_postButton]-|" options:0 metrics:nil views:views]];
-    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-64-[_urlField]-20-[_postButton]" options:0 metrics:nil views:views]];
+    [self.view addConstraints:[NSLayoutConstraint constraintsWithVisualFormat:@"V:|-64-[_urlField]-20-[_titleField]-20-[_postButton]" options:0 metrics:nil views:views]];
+    
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(applicationDidBecomeActive:) name:UIApplicationDidBecomeActiveNotification object:nil];
+}
+
+- (void)applicationDidBecomeActive:(NSNotification *)notification
+{
+    MPMediaItem *item = [MPMusicPlayerController iPodMusicPlayer].nowPlayingItem;
+    NSLog(@"item = %@", item);
+    
+    self.titleField.text = [item valueForProperty:MPMediaItemPropertyTitle];
 }
 
 - (void)retrieveCurrentMediaItem
@@ -66,6 +80,7 @@ static NSString * const kPostURLKey = @"postURL";
     
     AVAssetExportSession *session = [AVAssetExportSession exportSessionWithAsset:asset presetName:AVAssetExportPresetAppleM4A];
     NSString *filename = [NSTemporaryDirectory() stringByAppendingPathComponent:@"export.m4a"];
+    [[NSFileManager defaultManager] removeItemAtPath:filename error:nil];
     session.outputURL = [NSURL fileURLWithPath:filename];
     session.outputFileType = session.supportedFileTypes.firstObject;
     [session exportAsynchronouslyWithCompletionHandler:^{
